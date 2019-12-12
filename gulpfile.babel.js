09b05804532da920b9c00f684c73c1dd378cc1c8
PureDevPer <wooseok.kim1207@gmail.com>
2019-12-11 19:59:38 -0500

    Added Browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
 
 sass.compiler = require("node-sass");
 
@@ -10,8 +12,15 @@ const paths = {
     src: "assets/scss/styles.scss",
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss"
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js"
   }
 };
+
+const clean = () => del(["src/static"]);
 
 const styles = () => {
   return gulp
@@ -27,8 +36,17 @@ const styles = () => {
     .pipe(gulp.dest(paths.styles.dest));
 };
 
-const watchFiles = () => gulp.watch(paths.styles.watch, styles);
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(bro())
+    .pipe(gulp.dest(paths.js.dest));
 
-const dev = gulp.series([styles, watchFiles]);
+const watchFiles = () => {
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch, js);
+};
+
+const dev = gulp.series(clean, styles, js, watchFiles);
 
 export default dev;
